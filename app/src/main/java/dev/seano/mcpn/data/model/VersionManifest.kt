51cdeab62bd0a7acdafa5fd5baff9383b27da18b
Sean O'Connor <so@seano.dev>
2025-09-23 17:48:56 -0600

    Refactor VersionManifest to remove nested models
    You cannot pass nested serializable into androidx navigation safe args, so this is just a bit of future proofing.

--- app/src/main/java/dev/seano/mcpn/data/model/VersionManifest.kt
+++ app/src/main/java/dev/seano/mcpn/data/model/VersionManifest.kt
@@ -5,23 +5,23 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class VersionManifest(
-    @SerialName("latest") val latest: Latest,
-    @SerialName("versions") val versions: List<Version>
-) {
-    @Serializable
-    data class Latest(
-        @SerialName("release") val release: String,
-        @SerialName("snapshot") val snapshot: String
-    )
+    @SerialName("latest") val latest: VersionManifestLatest,
+    @SerialName("versions") val versions: List<VersionManifestVersion>
+)
 
-    @Serializable
-    data class Version(
-        @SerialName("complianceLevel") val complianceLevel: Int,
-        @SerialName("id") val id: String,
-        @SerialName("releaseTime") val releaseTime: String,
-        @SerialName("sha1") val sha1: String,
-        @SerialName("time") val time: String,
-        @SerialName("type") val type: String,
-        @SerialName("url") val url: String
-    )
-}
+@Serializable
+data class VersionManifestLatest(
+    @SerialName("release") val release: String,
+    @SerialName("snapshot") val snapshot: String
+)
+
+@Serializable
+data class VersionManifestVersion(
+    @SerialName("complianceLevel") val complianceLevel: Int,
+    @SerialName("id") val id: String,
+    @SerialName("releaseTime") val releaseTime: String,
+    @SerialName("sha1") val sha1: String,
+    @SerialName("time") val time: String,
+    @SerialName("type") val type: String,
+    @SerialName("url") val url: String
+)
--- app/src/main/java/dev/seano/mcpn/network/ApiService.kt
+++ app/src/main/java/dev/seano/mcpn/network/ApiService.kt
@@ -2,6 +2,7 @@ package dev.seano.mcpn.network
 
 import dev.seano.mcpn.data.model.Version
 import dev.seano.mcpn.data.model.VersionManifest
+import dev.seano.mcpn.data.model.VersionManifestVersion
 import org.koin.core.annotation.Single
 
 @Single
@@ -14,6 +15,6 @@ class ApiService(private val networkService: NetworkService) {
     suspend fun fetchVersionManifest(): NetworkResponse<VersionManifest> =
         networkService.get<VersionManifest>(Endpoints.VERSION_MANIFEST)
 
-    suspend fun fetchVersion(version: VersionManifest.Version): NetworkResponse<Version> =
+    suspend fun fetchVersion(version: VersionManifestVersion): NetworkResponse<Version> =
         networkService.get<Version>(version.url)
 }

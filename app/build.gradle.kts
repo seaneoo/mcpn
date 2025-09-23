eb7cad760be12119b0335077850767aae1e82f7c
Sean O'Connor <so@seano.dev>
2025-09-23 16:18:51 -0600

    Add Koin and configure in MainApplication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -58,6 +59,12 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.androidx.compose)
+    implementation(libs.koin.androidx.compose.navigation)
+    implementation(libs.koin.annotations)
+    ksp(libs.koin.ksp.compiler)
 
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.cio)
--- app/src/main/java/dev/seano/mcpn/MainApplication.kt
+++ app/src/main/java/dev/seano/mcpn/MainApplication.kt
@@ -1,5 +1,24 @@
 package dev.seano.mcpn
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+import org.koin.core.context.startKoin
+import org.koin.ksp.generated.module
 
-class MainApplication : Application()
+@Module @ComponentScan("dev.seano.mcpn") class AppModule
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@MainApplication)
+            androidLogger()
+            modules(AppModule().module)
+        }
+    }
+}

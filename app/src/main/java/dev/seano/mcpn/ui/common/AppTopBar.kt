7de255fc23cda5f5d2e655da8201df0c4bad07c3
Sean O'Connor <so@seano.dev>
2025-10-06 12:28:21 -0600

    Replace TopAppBar with CenterAlignedTopAppBar in AppTopBar component

--- app/src/main/java/dev/seano/mcpn/ui/common/AppTopBar.kt
+++ app/src/main/java/dev/seano/mcpn/ui/common/AppTopBar.kt
@@ -1,9 +1,9 @@
 package dev.seano.mcpn.ui.common
 
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
@@ -14,7 +14,7 @@ import dev.seano.mcpn.ui.theme.monocraft
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppTopBar() {
-    TopAppBar(
+    CenterAlignedTopAppBar(
         title = {
             Text(
                 stringResource(R.string.app_name),
